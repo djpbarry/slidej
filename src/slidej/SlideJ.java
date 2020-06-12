6f962b24ba3aa05eb754f54b5928d4b319c0b084
Dave Barry <david.barry@crick.ac.uk>
2020-06-12 19:28:01 +0100

    Organised outputs into separate directories

--- src/slidej/SlideJ.java
+++ src/slidej/SlideJ.java
@@ -10,13 +10,17 @@ import net.imagej.axis.CalibratedAxis;
 import net.imagej.axis.DefaultAxisType;
 import net.imagej.axis.DefaultLinearAxis;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.algorithm.gauss3.Gauss3;
 import net.imglib2.img.Img;
+import net.imglib2.img.ImgFactory;
 import net.imglib2.img.ImgView;
+import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.NumericType;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 import slidej.analysis.Analyser;
 import slidej.io.ImageLoader;
@@ -30,6 +34,8 @@ import java.util.List;
 public class SlideJ {
 
     public static String TITLE = "SlideJ v1.0";
+    private final String BINARIES = "binaries";
+    private final String AUX_INPUTS = "aux_inputs";
 
     public SlideJ() {
 
@@ -64,7 +70,15 @@ public class SlideJ {
         calNeighbourhood[caxis] = 1;
         calibrations[caxis] = 2;
 
-        generateBinariesAndMaps(img, caxis, calibrations);
+        if (!makeOutputDirectories(new File(file.getParent()), BINARIES, AUX_INPUTS)) {
+            System.out.print("Failed to create output directories- aborting.");
+            return;
+        }
+
+        generateBinariesAndMaps(img,
+                String.format("%s%s%s", file.getParent(), File.separator, BINARIES),
+                String.format("%s%s%s", file.getParent(), File.separator, AUX_INPUTS),
+                caxis, calibrations);
 
         Analyser<T> a = new Analyser<>(calNeighbourhood, dimLabels, calibrations);
         a.analyse(img);
@@ -81,33 +95,45 @@ public class SlideJ {
         }
     }
 
-    public <T extends RealType<T> & NativeType<T>> void generateBinariesAndMaps(Img<T> img, int caxis, double[] calibrations) {
+    public <T extends RealType<T> & NativeType<T>> void generateBinariesAndMaps(Img<T> img, String binOutDir, String mapOutDir, int caxis, double[] calibrations) {
         long[] dims = new long[img.numDimensions()];
         img.dimensions(dims);
 
+        double[] channelCals = new double[calibrations.length - 1];
+        System.arraycopy(calibrations, 0, channelCals, 0, caxis);
+        System.arraycopy(calibrations, caxis + 1, channelCals, caxis, channelCals.length - caxis);
+
+        long[] channelDims = new long[img.numDimensions() - 1];
+        System.arraycopy(dims, 0, channelDims, 0, caxis);
+        System.arraycopy(dims, caxis + 1, channelDims, caxis, channelDims.length - caxis);
+
+//        ImgFactory<FloatType> factory = new CellImgFactory<>(new FloatType());
+
         for (int c = 0; c < dims[caxis]; c++) {
-
-            ImageThresholder<T> it = new ImageThresholder<>(ImgView.wrap(Views.hyperSlice(img, caxis, c), img.factory()), "Default");
-            it.threshold();
-
-            Img<BitType> binary = it.getOutput();
-
-            saveImage(String.format("E:/Dropbox (The Francis Crick)/Antoniana's Data/TestOut/threshold_%d.tif", c),
+//            Img<FloatType> filtered = factory.create(channelDims);
+            RandomAccessibleInterval<T> channel = Views.hyperSlice(img, caxis, c);
+//            Gauss3.gauss(2.0, ImgView.wrap(channel, img.factory()), filtered);
+//            Img<BitType> binary = thresholdImg(filtered, "Default");
+            Img<BitType> binary = thresholdImg(ImgView.wrap(channel, img.factory()), "Default");
+            saveImage(String.format("%S%Sthreshold_%d.tif", binOutDir, File.separator, c),
                     (new ImageJ()).op().convert().uint8(binary));
 
             long[] binDims = new long[binary.numDimensions()];
             binary.dimensions(binDims);
 
-            double[] channelCals = new double[calibrations.length - 1];
-            System.arraycopy(calibrations, 0, channelCals, 0, caxis);
-            System.arraycopy(calibrations, caxis + 1, channelCals, caxis, channelCals.length - caxis);
-
-            saveImage(String.format("E:/Dropbox (The Francis Crick)/Antoniana's Data/TestOut/distanceMap_%d.tif", c),
+            saveImage(String.format("%s%sdistanceMap_%d.tif", mapOutDir, File.separator, c),
                     DistanceTransformer.calcDistanceMap(binary, channelCals, binDims, false));
 
-            saveImage(String.format("E:/Dropbox (The Francis Crick)/Antoniana's Data/TestOut/invertedDistanceMap_%d.tif", c),
+            saveImage(String.format("%s%sinvertedDistanceMap_%d.tif", mapOutDir, File.separator, c),
                     DistanceTransformer.calcDistanceMap(binary, channelCals, binDims, true));
         }
+    }
+
+    public <T extends RealType<T> & NativeType<T>> Img<BitType> thresholdImg(Img<T> img, String method) {
+        ImageThresholder<T> it = new ImageThresholder<>(img, method);
+        it.threshold();
+
+        return it.getOutput();
     }
 
     public <T extends NumericType<T> & NativeType<T>> void showImage(RandomAccessibleInterval<T> img) {
@@ -118,4 +144,19 @@ public class SlideJ {
         (new ImgSaver()).saveImg(path, img);
     }
 
+    private boolean makeOutputDirectories(File parent, String... children) {
+        boolean success = true;
+        for (String path : children) {
+            success = success && makeOutputDirectory(String.format("%s%s%s", parent.getAbsolutePath(), File.separator, path));
+        }
+        return success;
+    }
+
+    private boolean makeOutputDirectory(String path) {
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        return true;
+    }
 }
