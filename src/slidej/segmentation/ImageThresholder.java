e6fa86cc1aa00e6f444cb81e11901876b9b95f8f
Dave Barry <david.barry@crick.ac.uk>
2020-06-23 16:17:54 +0100

    Added ImageThresholder.thresholdImage

--- src/slidej/segmentation/ImageThresholder.java
+++ src/slidej/segmentation/ImageThresholder.java
@@ -1,13 +1,12 @@
 package slidej.segmentation;
 
 import ij.process.AutoThresholder;
-import net.imagej.ImageJ;
 import net.imglib2.algorithm.stats.ComputeMinMax;
 import net.imglib2.algorithm.stats.Histogram;
 import net.imglib2.algorithm.stats.RealBinMapper;
 import net.imglib2.cache.img.DiskCachedCellImgFactory;
 import net.imglib2.img.Img;
-import net.imglib2.img.ImgView;
+import net.imglib2.loops.LoopBuilder;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
@@ -31,11 +30,22 @@ public class ImageThresholder<T extends RealType<T> & NativeType<T>> {
         hist.process();
         int threshBin = (new AutoThresholder()).getThreshold(method, hist.getHistogram());
 
-        (new ImageJ()).op().threshold().apply(output, input, hist.getBinCenter(threshBin));
+        //(new ImageJ()).op().threshold().apply(output, input, hist.getBinCenter(threshBin));
+
+        thresholdImage(hist.getBinCenter(threshBin));
 
         //output = Thresholder.threshold(input, hist.getBinCenter(threshBin), true, Runtime.getRuntime().availableProcessors());
         System.out.println("Image thresholded.");
         System.out.println(String.format("%.1f GB of RAM free.", Runtime.getRuntime().freeMemory() / 1e+9));
+    }
+
+    private void thresholdImage(T threshold) {
+        BitType fg = new BitType();
+        BitType bg = new BitType();
+        fg.set(true);
+        bg.set(false);
+
+        LoopBuilder.setImages(input, output).forEachPixel((in, out) -> out.set(in.getRealFloat() > threshold.getRealFloat() ? fg : bg));
     }
 
     public Img<BitType> getOutput() {
